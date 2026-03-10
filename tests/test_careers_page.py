2f0322e4c40aeff6b89b23a77986920a838b35d5
berkay.barlas <berkay.barlas@bitaksi.com>
2026-03-10 22:52:47 +0300

    feat: add get_job_cards_data method to CareersPage

--- tests/test_careers_page.py
+++ tests/test_careers_page.py
@@ -1,50 +1,42 @@
+import pytest
+
+from data.expected_content import EXPECTED_JOB_DEPARTMENT, EXPECTED_JOB_LOCATION
 from flows.site_flow import SiteFlow
 from pages.careers_page import CareersPage
 
 
 class TestCareersPage:
 
-        def test_careers_page(self, driver):
-            careers_page = CareersPage(driver)
-            site_flow = SiteFlow(careers_page)
+    @pytest.fixture
+    def qa_filtered_careers_page(self, driver):
+        careers_page = CareersPage(driver)
+        site_flow = SiteFlow(careers_page)
+        careers_page.open_page()
+        site_flow.handle_cookie_banner("accept_all")
+        careers_page.click_see_all_teams()
+        careers_page.wait_for_positions_to_load()
+        careers_page.click_open_qa_positions()
+        assert careers_page.is_quality_assurance_filter_selected(), \
+            "Quality Assurance filter is not selected after clicking QA positions."
+        assert careers_page.are_job_postings_present(), \
+            "No job postings were found for Quality Assurance filter."
+        return careers_page
 
-            careers_page.open_page()
-            site_flow.handle_cookie_banner("only_necessary")
-            careers_page.click_see_all_teams()
-            careers_page.wait_for_positions_to_load()
-            careers_page.click_open_qa_positions()
-            careers_page.is_quality_assurance_filter_selected()
-            assert careers_page.are_job_postings_present(), \
-                "No job postings were found for Quality Assurance filter."
+    def test_qa_jobs_match_position_and_location(self, qa_filtered_careers_page):
+        #qa_filtered_careers_page.select_location()
+        #qa_filtered_careers_page.validate_job_cards()
 
-        def test_qa_jobs_match_expected_position_department_and_location(self, driver):
-            careers_page = CareersPage(driver)
-            site_flow = SiteFlow(careers_page)
+        qa_filtered_careers_page.select_location()
+        job_cards = qa_filtered_careers_page.get_job_cards_data()
 
-            careers_page.open_page()
-            site_flow.handle_cookie_banner("only_necessary")
-            careers_page.click_see_all_teams()
-            careers_page.wait_for_positions_to_load()
-            careers_page.click_open_qa_positions()
-            careers_page.is_quality_assurance_filter_selected()
-            assert careers_page.are_job_postings_present(), \
-                "No job postings were found for Quality Assurance filter."
-            careers_page.select_location()
-            careers_page.validate_job_cards()
+        for job in job_cards:
+            assert EXPECTED_JOB_DEPARTMENT in job["title"], \
+                f"Unexpected job title: '{job['title']}'"
+            assert EXPECTED_JOB_LOCATION in job["location"], \
+                f"Unexpected job location: '{job['location']}'"
 
-        def test_qa_jobs_filter_results_and_view_role_redirect(self, driver):
-            careers_page = CareersPage(driver)
-            site_flow = SiteFlow(careers_page)
-
-            careers_page.open_page()
-            site_flow.handle_cookie_banner("only_necessary")
-            careers_page.click_see_all_teams()
-            careers_page.wait_for_positions_to_load()
-            careers_page.click_open_qa_positions()
-            careers_page.is_quality_assurance_filter_selected()
-            assert careers_page.are_job_postings_present(), \
-                "No job postings were found for Quality Assurance filter."
-            careers_page.select_location()
-            careers_page.validate_job_cards()
-            careers_page.click_apply_button()
-            careers_page.is_view_role_redirected_to_expected_lever_url()
+    def test_qa_jobs_filter_and_view_role_redirect(self, qa_filtered_careers_page):
+        qa_filtered_careers_page.select_location()
+        qa_filtered_careers_page.validate_job_cards()
+        qa_filtered_careers_page.click_apply_button()
+        assert qa_filtered_careers_page.is_view_role_redirected_to_expected_lever_url()
