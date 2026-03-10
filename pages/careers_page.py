e0f31247d6824b250ae995b7b8e9f0f674a7924d
berkay.barlas <berkay.barlas@bitaksi.com>
2026-03-10 13:35:55 +0300

    fix: correct QA filter validation by using visible selected filter label

---
+++ pages/careers_page.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.support.wait import WebDriverWait
+
+from data.urls import CAREERS_URL
+from locators.careers_page_locators import CareersPageLocators
+from pages.base_page import BasePage
+
+
+class CareersPage(BasePage):
+
+    def open_page(self):
+        self.open(CAREERS_URL)
+
+    def click_see_all_teams(self):
+        self.click(CareersPageLocators.SEE_ALL_TEAMS_BUTTON)
+
+    def click_open_qa_positions(self):
+        self.click(CareersPageLocators.QUALITY_ASSURANCE_POSITIONS_CARD)
+
+    def wait_for_positions_to_load(self):
+        self.logger.info("Waiting for QA positions count to be greater than 0")
+
+        WebDriverWait(self.driver, 10).until(
+            lambda driver: int(
+                driver.find_element(*CareersPageLocators.QUALITY_ASSURANCE_POSITIONS_CARD)
+                .text.split()[0]
+            ) > 0
+        )
+
+    def is_quality_assurance_filter_selected(self):
+        element = self.wait_for_visibility(
+            CareersPageLocators.SELECTED_TEAM_FILTER_LABEL
+        )
+        return "Quality Assurance" in element.text
