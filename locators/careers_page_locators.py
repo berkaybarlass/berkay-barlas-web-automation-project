2ed35efd8627cc252a8a6704b0bf7161975cfc55
berkay.barlas <berkay.barlas@bitaksi.com>
2026-03-10 12:37:19 +0300

    feat: add and update page locators

--- locators/careers_page_locators.py
+++ locators/careers_page_locators.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
 
 class CareersPageLocators:
-    SEE_ALL_TEAMS_BUTTON = (By.CSS_SELECTOR, ".see-more")
-    QUALITY_ASSURANCE_POSITIONS_CARD = ".elementor-repeater-item-56ca501 .insiderone-icon-cards-grid-item-btn"
+    SEE_ALL_TEAMS_BUTTON = (By.XPATH, "//a[contains(.,'See all teams')]")
+    QUALITY_ASSURANCE_POSITIONS_CARD = (By.CSS_SELECTOR, ".elementor-repeater-item-56ca501 .insiderone-icon-cards-grid-item-btn")
