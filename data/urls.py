e7a951f777c69ad4e20f470e6e4fbf0d8b51bbb4
berkay.barlas <berkay.barlas@bitaksi.com>
2026-03-10 18:19:16 +0300

    refactor: centralize base URL in urls.py and remove duplicate config entry

--- data/urls.py
+++ data/urls.py
@@ -1,2 +1,3 @@
 BASE_URL = "https://insiderone.com/"
 CAREERS_URL = "https://insiderone.com/careers/#open-roles"
+LEVER_QA_JOB_URL = "https://jobs.lever.co/insiderone/774658ce-0d6e-4b07-a69b-4629fa11d6f3"
